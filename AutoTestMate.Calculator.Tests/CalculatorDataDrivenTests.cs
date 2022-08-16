4955ebb7224624b01a36a7b9e39563dfabb92150
Joey Turks <sportyturks@gmail.com>
2022-08-16 15:46:28 +1000

    add Dynamic data examples

--- AutoTestMate.Calculator.Tests/CalculatorDataDrivenTests.cs
+++ AutoTestMate.Calculator.Tests/CalculatorDataDrivenTests.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Reflection;
 using AutoTestMate.Calculator.Models;
 using AutoTestMate.MsTest.Web.Core;
 using AutoTestMate.MsTest.Web.Extensions;
@@ -26,5 +28,61 @@ public class CalculatorDataDrivenTests : WebTestBase
             .Open()
             .Calculate(ops)
             .AssertValue(expected);
+    }
+
+    [TestMethod]
+    [DynamicData(nameof(GetDynamicData), DynamicDataSourceType.Method)]
+    public void CalculateDynamicDataTest(string ops, double expected, string _)
+    {
+
+        LoggingUtility.Info($"Operation: {ops.Replace(",", " ")} = {expected}", true);
+
+        var calcPage = GetPage<CalculatorPage>(TestMethod)
+            .Open()
+            .Calculate(ops)
+            .AssertValue(expected);
+    }
+    
+    [TestMethod]
+    [DynamicData(nameof(GetDynamicData2), DynamicDataSourceType.Method, DynamicDataDisplayName = nameof(GetTestDisplayNames))]
+    public void CalculateDynamicDataSpecificNameTest(string testName, string ops, double expected)
+    {
+
+        LoggingUtility.Info($"Operation: {ops.Replace(",", " ")} = {expected}", true);
+
+        var calcPage = GetPage<CalculatorPage>(TestMethod)
+            .Open()
+            .Calculate(ops)
+            .AssertValue(expected);
+    }
+
+    public static IEnumerable<object[]> GetDynamicData()
+    {
+        return new[]
+        {
+            new object[] { "1,+,1", 2, "TC_Calc_1" },
+            new object[] { "1,3,-,4", 9, "TC_Calc_2" },
+            new object[] { "2,*,3", 6, "TC_Calc_3" },
+            new object[] { "1,0,0,/,2,5", 4, "TC_Calc_4" },
+            new object[] { "1,+,2,*,(,4,-,6,)", -3, "TC_Calc_5" },
+            new object[] { "4,*,(,3,-,4,/,2,)", 4, "TC_Calc_6" }
+        };
+    }
+    public static IEnumerable<object[]> GetDynamicData2()
+    {
+        return new[]
+        {
+            new object[] { "TC_Calc_1", "1,+,1", 2 },
+            new object[] { "TC_Calc_2", "1,3,-,4", 9,},
+            new object[] { "TC_Calc_3", "2,*,3", 6},
+            new object[] { "TC_Calc_4", "1,0,0,/,2,5", 4 },
+            new object[] { "TC_Calc_5", "1,+,2,*,(,4,-,6,)", -3 },
+            new object[] { "TC_Calc_6", "4,*,(,3,-,4,/,2,)", 4 }
+        };
+    }
+    public static string GetTestDisplayNames(MethodInfo methodInfo, object[] values)
+    {
+        var name = (string)values[0];
+        return $"{methodInfo.Name}({name})";
     }
 }
